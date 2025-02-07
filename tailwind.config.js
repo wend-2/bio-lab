c606a1dcfb0ef54a06fdeaf50dd25f248ee8f363
Berwyn-s <berwyn.surya007@gmail.com>
2025-02-07 14:25:25 +0700

    Initiated tailwind on the project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'dark-purple' : '#161B40',
+        'light-blue' : '#43BEE5',
+        'soft-red' : '#F40058',
+        'soft-orange' : '#EFA500',
+        'soft-green' : '#41B853'
+      }
+    },
+  },
+  plugins: [],
+}
+
